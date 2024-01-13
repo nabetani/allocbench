215fa808492ef0a150856c1afe772ed14762e1b0
Nabetani <takenori@nabetani.sakura.ne.jp>
2024-01-13 15:20:07 +0900

    29で止めるなど

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -3,33 +3,82 @@
 #include <cstddef>
 #include <memory>
 #include <chrono>
-
+#include <map>
+#include <vector>
+#include <string>
 
 using std_clock = std::chrono::high_resolution_clock;
+using namespace std::literals::string_literals;
 
 void test_m(int i){
     auto t0 = std_clock::now();
-    auto p = malloc( 1<<i );
+    auto p = malloc( size_t(i)<<i );
     auto t1 = std_clock::now();
     auto diff_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(t1-t0).count();
-    printf( "m %d, %f, %p\n", i, diff_ns*1e-6, p );
+    printf( "m, %d, %f, %p\n", i, diff_ns*1e-6, p );
     free(p);
 }
 
 void test_c(int i){
     auto t0 = std_clock::now();
-    auto p = calloc( 1<<i, 1 );
+    auto p = calloc( size_t(i)<<i, 1 );
     auto t1 = std_clock::now();
     auto diff_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(t1-t0).count();
-    printf( "c %d, %f, %p\n", i, diff_ns*1e-6, p );
+    printf( "c, %d, %f, %p\n", i, diff_ns*1e-6, p );
     free(p);
 }
 
-int main(){
-    for( int trial=0 ; trial<5 ; ++trial ){
-        for( int i=10 ; i<=31 ; ++i ){
-            test_m(i);
-            test_c(i);
-        }
+void test_v(int i){
+    auto t0 = std_clock::now();
+    try{
+        auto p = std::vector<char>(size_t(i)<<i);
+        auto t1 = std_clock::now();
+        auto diff_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(t1-t0).count();
+        printf( "v, %d, %f, %p\n", i, diff_ns*1e-6, p.data() );
+    }
+    catch( std::exception & e ){
+        printf( "v, %d, fail\n", i);
+    }
+}
+
+void test_r(int i){
+    auto t0 = std_clock::now();
+    try{
+        auto p = std::vector<char>();
+        p.reserve(size_t(i)<<i);
+        auto t1 = std_clock::now();
+        auto diff_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(t1-t0).count();
+        printf( "v, %d, %f, %p\n", i, diff_ns*1e-6, p.data() );
+    }
+    catch( std::exception & e ){
+        printf( "v, %d, fail\n", i);
+    }
+}
+
+void test_s(int i){
+    auto t0 = std_clock::now();
+    try{
+        auto p = std::vector<char>();
+        p.resize(size_t(i)<<i);
+        auto t1 = std_clock::now();
+        auto diff_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(t1-t0).count();
+        printf( "v, %d, %f, %p\n", i, diff_ns*1e-6, p.data() );
+    }
+    catch( std::exception & e ){
+        printf( "v, %d, fail\n", i);
+    }
+}
+
+int main(int argc, char const * argv[]){
+    typedef void (*tester_t)(int);
+    tester_t test = std::map<std::string,tester_t>({
+        { "c"s, test_c },
+        { "m"s, test_m },
+        { "v"s, test_v },
+        { "r"s, test_r },
+        { "s"s, test_s },
+    })[argv[1]];
+    for( int i=1 ; i<=29 ; ++i ){
+        test(i);
     }
 }
--- goma/main.go
+++ goma/main.go
@@ -2,20 +2,51 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"runtime"
 	"time"
 )
 
-func test(i int) {
+func test_l(i int) {
 	t0 := time.Now()
+	defer func() {
+		err := recover()
+		if err != nil {
+			fmt.Printf("l, %v, failed: %v\n", i, err)
+		}
+	}()
 	p := make([]uint8, 1<<i)
 	diff := float64(time.Since(t0)) * 1e-6
-	fmt.Printf("%v, %#v, %p\n", i, diff, &p)
+	fmt.Printf("l, %v, %#v, %p\n", i, diff, &p)
+}
+
+func test_c(i int) {
+	t0 := time.Now()
+	defer func() {
+		err := recover()
+		if err != nil {
+			fmt.Printf("c, %v, failed: %v\n", i, err)
+		}
+	}()
+	p := make([]uint8, 0, 1<<i)
+	diff := float64(time.Since(t0)) * 1e-6
+	fmt.Printf("c, %v, %#v, %p\n", i, diff, &p)
+}
+
+func gc() {
+	runtime.GC()
+	time.Sleep(time.Second)
 }
 
 func main() {
-	for trial := 0; trial < 5; trial++ {
-		for i := 10; i <= 31; i++ {
-			test(i)
+	proc := func() func(int) {
+		if 1 < len(os.Args) && os.Args[0] == "c" {
+			return test_c
 		}
+		return test_l
+	}()
+	for i := 0; i <= 29; i++ {
+		proc(i)
+		gc()
 	}
 }
